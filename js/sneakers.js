1d1f88bcdc41a9e006d0bb6e3a55ff87035c023c
daniela <yucuma13@gmail.com>
2023-11-03 07:27:50 -0500

    sneakers1

--- js/sneakers.js
+++ js/sneakers.js
@@ -11,24 +11,15 @@ let thumb2 = document.getElementById("thumb2");
 let thumb3 = document.getElementById("thumb3");
 let thumb4 = document.getElementById("thumb4");
 let botonCerrar = document.getElementById("botonCerrar");
-/* let numeroImagen = 0; */
+let boxMinus = document.getElementById("boxMinus");
+let boxCantidad = document.getElementById("boxCantidad");
+let boxPlus = document.getElementById("boxPlus");
+let valorUnitario = 180000; 
+
 
 mainImg.addEventListener("click", function() {
     boxImgFixed.classList.remove("box-img-fixed-hidden");
     boxImgFixed.classList.add("box-img-fixed");
-    /* mainImgFixed1.classList.remove(""); */
-/*         mainImgFixed1.classList.add("img-hidden");
-        mainImgFixed2.classList.remove("img-hidden");
-    }
-    else if (!mainImgFixed2.classList.contains("img-hidden")) {
-        mainImgFixed2.classList.add("img-hidden");
-        mainImgFixed3.classList.remove("img-hidden");
-    }
-    else if (!mainImgFixed3.classList.contains("img-hidden")) {
-        mainImgFixed3.classList.add("img-hidden");
-        mainImgFixed4.classList.remove("img-hidden");
-    } */
-    
 });
 iconRight.addEventListener("click", function() {
     if (!mainImgFixed1.classList.contains("img-hidden")) {
@@ -93,5 +84,28 @@ botonCerrar.addEventListener("click", function() {
     boxImgFixed.classList.add("box-img-fixed-hidden");
     boxImgFixed.classList.remove("box-img-fixed");
 });
-/* let fecha = new Date();
-alert(fecha.getDate().toString() + " " +fecha.getFullYear().toString()); */
+/* Mostrando el valor del producto */
+let valorUnitarioAux = Intl.NumberFormat("DE-de").format(valorUnitario);
+document.getElementById("precioUnitario").innerHTML = "$" + valorUnitarioAux;
+
+boxPlus.addEventListener("click",function(){
+    let cantidad = boxCantidad.innerHTML;
+    if (cantidad < 10){
+    cantidad ++;
+    document.getElementById("boxCantidad").innerHTML = cantidad;
+    let valorTotalInt = cantidad * valorUnitario;
+    let valorTotalAux = Intl.NumberFormat ("DE-de").format(valorTotalInt);
+    document.getElementById("valorTotal").innerHTML = "$" + valorTotalAux;
+}
+});
+
+boxMinus.addEventListener("click",function(){
+    let cantidad = boxCantidad.innerHTML;
+    if (cantidad > 0){
+    cantidad --;
+    document.getElementById("boxCantidad").innerHTML = cantidad;
+    let valorTotalInt = cantidad * valorUnitario;
+    let valorTotalAux = Intl.NumberFormat ("DE-de").format(valorTotalInt);
+    document.getElementById("valorTotal").innerHTML = "$" + valorTotalAux;
+}
+});
